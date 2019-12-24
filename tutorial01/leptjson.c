330d355aa0f78619b7fb20d1f22a9045ce2dedac
liqiang <gymbo@gymbodeMacBook-Pro.local>
2019-12-24 18:14:31 +0800

    1

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -24,12 +24,46 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
     return LEPT_PARSE_OK;
 }
 
+static int lept_parse_true(lept_context* c, lept_value* v) {
+    EXPECT(c, 't');
+    if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 3;
+    v->type = LEPT_TRUE;
+    return LEPT_PARSE_OK;
+}
+
+static int lept_parse_false(lept_context* c, lept_value* v) {
+    EXPECT(c, 'f');
+    if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 4;
+    v->type = LEPT_FALSE;
+    return LEPT_PARSE_OK;
+}
+
 static int lept_parse_value(lept_context* c, lept_value* v) {
+    int res;
     switch (*c->json) {
-        case 'n':  return lept_parse_null(c, v);
+        case 'n':
+            res = lept_parse_null(c, v);
+            break;
+        case 't':
+            res = lept_parse_true(c, v);
+            break;
+        case 'f':
+            res = lept_parse_false(c, v);
+            break;
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
         default:   return LEPT_PARSE_INVALID_VALUE;
     }
+
+    if (res != LEPT_PARSE_OK) return res;
+
+    lept_parse_whitespace(c);
+    if (*c->json != '\0') return LEPT_PARSE_ROOT_NOT_SINGULAR;
+
+    return res;
 }
 
 int lept_parse(lept_value* v, const char* json) {
